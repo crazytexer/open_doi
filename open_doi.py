7fd9243601e4df64410c3642d8eef424c13004b4
crazytexer <62206894+crazytexer@users.noreply.github.com>
2021-03-18 20:13:32 +0300

    change docstring

--- open_doi.py
+++ open_doi.py
@@ -10,13 +10,13 @@ import webbrowser
 
 
 class OpenDoiCommand(sublime_plugin.TextCommand):
-    """Open the selected DOI as an URL in your browser."""
+    '''Open the DOI, selected in Sublime Text, as an URL in your browser.'''
     def run(self, edit):
         opened = False
         for region in self.view.sel():
             doi = self.view.substr(region).strip()
             if doi.startswith('10.') and ('/' in doi):
-                webbrowser.open("https://doi.org/" + doi)
+                webbrowser.open('https://doi.org/' + doi)
                 opened = True
         if not opened:
-            sublime.status_message("Wrong DOI syntax!")
+            sublime.status_message('Wrong DOI syntax!')
